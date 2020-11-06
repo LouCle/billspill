78fb6badc9ecfbd8a7210827598cdc4bd5110aea
LouCle <clementmagnus@gmail.com>
2020-11-06 18:21:31 +0100

    brug klasse

--- index.js
+++ index.js
@@ -3,40 +3,39 @@ let H = window.innerHeight
 let biller = []
 
 
-function Bille(x, y, mvel, mrot, fspeed, r, iq, eyes, mouth) {
-    this.pos = createVector(x,y)
-    this.vel = createVector(0,0)
-    this.rot = 0
-    this.mvel = mvel
-    this.mrot = mrot
-    this.fspeed = fspeed //firing speed
-    this.r = r
-    this.iq = iq //noget med ai
-    this.eyes = eyes
-    this.mouth = mouth
-
-    this.update = function() {
-        // this
+class Bille {
+    constructor(x, y, mvel, mrot, fspeed, r, iq, eyes, mouth, team) {
+        this.pos = createVector(x,y)
+        this.vel = createVector(0,0)
+        this.rot = 0
+        this.mvel = mvel
+        this.mrot = mrot
+        this.fspeed = fspeed //firing speed
+        this.r = r
+        this.iq = iq //noget med ai
+        this.eyes = eyes
+        this.mouth = mouth
+        this.team = 0
     }
 
-    this.show = function() {
-        fill(255)
-        ellipse(this.pos.x, this.pos.y, this.r, this.r)
+    update() {
+        
     }
+
+    render() {
+
+    }
+    
 }
 
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight)
     background(0)
-
-    
     
 }
 
 function draw() {
     background(0)
-    for (let bill in biller) {
-        
-    }
+    updateEntities()
 }
