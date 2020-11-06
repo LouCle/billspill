b7dd169cdef17ae8c158f8e5ad894f2fc0ac3581
Havkost <August.kuhn@gmail.com>
2020-11-06 22:18:20 +0100

    Render beetles to screen and change a few variable names

--- bille.js
+++ bille.js
@@ -1,16 +1,17 @@
 class Bille {
-    constructor(x, y, mvel, mrot, fspeed, r, iq, eyes, mouth, team) {
-        this.pos = createVector(x,y)
-        this.vel = createVector(0,0)
-        this.rot = 0
-        this.mvel = mvel
-        this.mrot = mrot
-        this.fspeed = fspeed //firing speed
-        this.r = r
-        this.iq = iq //noget med ai
-        this.eyes = eyes
-        this.mouth = mouth
-        this.team = 0
+    constructor(x, y, mvel, mrot, firerate, size, iq, eyes, mouth, team) {
+        this.pos = createVector(x,y);
+        this.vel = createVector(0,0);
+        this.acc = createVector(0,0);
+        this.rot = 0;
+        this.mvel = mvel; // Is this how fast it can move?
+        this.mrot = mrot; // And is this how fast it can rotate?
+        this.firerate = firerate;
+        this.size = size;
+        this.iq = iq; //noget med ai
+        this.eyes = eyes;
+        this.mouth = mouth;
+        this.team = team;
     }
 
     update() {
@@ -20,7 +21,9 @@ class Bille {
     }
 
     render() {
-        ellipse(this.pos.x, this.pos.y, this.r);
+        fill(this.team*255);
+        noStroke();
+        ellipse(this.pos.x, this.pos.y, this.size);
     }
     
 }
--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 // Main file
 // Initialize global variables
-let W = window.innerWidth
-let H = window.innerHeight
-let biller = []
+let W = window.innerWidth;
+let H = window.innerHeight;
+let borderW = 100;
+let borderH = 100;
+let biller = [];
 
 // Setup function (runs once)
 function setup() {
     createCanvas(W, H);
     background(0);
     
+    for(let i = 0; i < 6; i++){
+        biller[i] = new Bille(random(borderW, W-borderW), random(borderH, H-borderH), 5, 5, 5, 50, 500, 5, 5, i%2);
+    }
+}
+
+// Function for updating entities (duh)
+function updateEntities() {
+    // Split these into two, as I think doing them both at once for each bug could lead to dead bugs being alive for an extra frame, probably not worth splitting the loop for that but w/e I'm tired - H
+    for(let i = 0; i < biller.length; i++){
+        biller[i].update();
+    }
+    for(let i = 0; i < biller.length; i++){
+        biller[i].render();
+    }
 }
 
 // Draw function (runs in a loop)
 function draw() {
-    background(0)
-    updateEntities()
+    background(color(50,150,100));
+    updateEntities();
 }
